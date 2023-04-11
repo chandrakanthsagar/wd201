5398a4df0a814fefcb7392f217a97d747debd3f7
chandrakanthsagar <109515216+chandrakanthsagar@users.noreply.github.com>
2023-04-11 15:29:49 +0530

    hlo ram

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -23,14 +23,14 @@ module.exports = (sequelize, DataTypes) => {
       const OverdueItems=await Todo.overdue();
       const todolistItems=OverdueItems.map(todos => todos.displayableString()).join("\n");
       console.log(todolistItems);
-      console.log("\n");
+      console.log("");
 
       console.log("Due Today");
       // FILL IN HERE
       const DueItemsToday=await Todo.dueToday();
       const todolistItems1=DueItemsToday.map(todos=> todos.displayableString()).join("\n");
       console.log(todolistItems1);
-      console.log("\n");
+      console.log("");
 
       console.log("Due Later");
       // FILL IN HERE
