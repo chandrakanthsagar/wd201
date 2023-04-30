188a5a2602c03b762e005f2304429a2adf214d3e
chandrakanthsagar <109515216+chandrakanthsagar@users.noreply.github.com>
2023-04-30 22:16:46 +0530

    Update pre-commit hook and index.js file

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-unused-vars */
+
 const app = require("./app");
-const port = process.env.PORT || 3000
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log("Started express server at port 3000");
 });
